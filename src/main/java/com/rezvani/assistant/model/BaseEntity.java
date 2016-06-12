662ccb0341513937af396dd46e96bd2e43032c6a
mrezvani <m.rezvani.rad@gmail.com>
2016-06-12 12:17:31 +0430

    class Diagram for model

---
+++ src/main/java/com/rezvani/assistant/model/BaseEntity.java
@@ -0,0 +1,18 @@
+package com.rezvani.assistant.model;
+
+/**
+ * Created by mrezvani on 6/12/2016.
+ */
+public class BaseEntity {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+
+
+    }
+}
