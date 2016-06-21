48db6ba693023058910f376636eb9b9cfdf5c0a0
mrezvani <m.rezvani.rad@gmail.com>
2016-06-21 14:10:34 +0430

    model class

---
+++ src/main/java/com/rezvani/assistant/dao/test.java
@@ -0,0 +1,7 @@
+package com.rezvani.assistant.dao;
+
+/**
+ * Created by mrezvani on 6/21/2016.
+ */
+public class test {
+}
---
+++ src/main/java/com/rezvani/assistant/view/test.java
@@ -0,0 +1,7 @@
+package com.rezvani.assistant.view;
+
+/**
+ * Created by mrezvani on 6/21/2016.
+ */
+public class test {
+}
